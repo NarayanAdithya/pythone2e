b8fd10568e80e095787b08add31a6f0526919dc4
NarayanAdithya <narayanadithya1234@gmail.com>
2022-12-15 01:36:20 +0530

    Corrected Mistakes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -4,4 +4,4 @@ from app import app
 def test_home():
     response = app.test_client().get('/')
     assert response.text == 'Flask Website Running Successfully'
-    assert response.status == '200OK'
+    assert response.status == '200 OK'
