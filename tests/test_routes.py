4dc887894642181412cdf31e5ba4eb86dee69b2b
NarayanAdithya <narayanadithya1234@gmail.com>
2022-12-15 16:45:07 +0530

    Added New Route

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,7 +1,13 @@
 from app import app
 
 
-def test_home():
+def test_homepage():
     response = app.test_client().get('/')
     assert response.text == 'Flask Website Running Successfully'
     assert response.status == '200 OK'
+
+
+def test_printname():
+    response = app.test_client().get('/adithya')
+    assert response.text == "Hey adithya"
+    assert response.status_code == 200
